858cf7f62ea0388fa7eb7c9039bc5d81e9d402bd
Murloc Murlockovich <murlocked@Murlocs-MacBook-Air.local>
2022-12-12 04:36:05 +0300

    Добавляю всякое

---
+++ calcbot.py
@@ -0,0 +1,81 @@
+import telebot
+import json
+
+token = "тут был токен"
+bot = telebot.TeleBot(token)
+
+# хэндлер для яндекс клауда
+def handler(event,context):
+    body = json.loads(event['body'])
+    update = telebot.types.Update.de_json(body)
+    bot.process_new_updates([update])
+
+def calc(new_str):
+    new_str = str(new_str)
+    new_list = []
+    pos = 0
+    for index, item in enumerate(new_str): # тут я разбиваю выражение на список из чисел и знаков e.g. ['14', '/', '2']
+        if item == '*' or item == '/' or item == '+' or item == '-':
+            new_list.append(new_str[pos:index])
+            new_list.append(item)
+            pos = index + 1
+    new_list.append(new_str[pos:])
+  
+    # 4 функции для вычисления
+    def div(index): # деление
+        new_list[index] = int(new_list[index - 1]) / int(new_list[index + 1])
+        new_list.pop(index - 1)
+        new_list.pop(index)
+        return new_list
+  
+    def mult(index): # умножение
+        new_list[index] = int(new_list[index - 1]) * int(new_list[index + 1])
+        new_list.pop(index - 1)
+        new_list.pop(index)
+        return new_list
+  
+    def add(index): # сложение
+        new_list[index] = int(new_list[index - 1]) + int(new_list[index + 1])
+        new_list.pop(index - 1)
+        new_list.pop(index)
+        return new_list
+  
+    def subtr(index): # вычитание
+        new_list[index] = int(new_list[index - 1]) - int(new_list[index + 1])
+        new_list.pop(index - 1)
+        new_list.pop(index)
+        return new_list
+  
+    # а тут цикл с вычилением; гоняет список по функциям сверху пока не придёт к ответу
+    while len(new_list) != 1: 
+        for index, item in enumerate(new_list):
+            if item == '*':
+                new_list = mult(index)
+            if item == '/':
+                new_list = div(index)
+            if '*' not in new_list and '/' not in new_list:
+                if item == '+':
+                    new_list = add(index)
+                if item == '-':
+                    new_list = subtr(index)
+    
+    return '{0:g}'.format(new_list[0]) #это чтобы .0 отбрасывать в конце чисел
+
+def listener(messages):
+    for m in messages:
+        chatid = m.chat.id
+        if m.content_type == 'text':
+            text = m.text
+            bot.send_message(chatid, calc(text))
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, '''Привет!
+    Это бот-калькулятор для тестового задания в Saber Interactive. Калькулятор может делать самые простые вычисления: сложение, вычитание, умножение, деление. Просто отправь любое выражение в чат после этого текста, специальные команды не нужны. Пример: 14+6/2*5*2-4''')
+
+bot.set_update_listener(listener)
+
+# бесконечный цикл получения новых записей со стороны Telegram
+
+if __name__ == '__main__':
+    bot.infinity_polling()
