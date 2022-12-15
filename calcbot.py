88ede9949efcd59c75b85db39f307cf811e54f30
Murloc Murlockovich <murlocked@Murlocs-MacBook-Air.local>
2022-12-15 20:48:31 +0300

    upd float

--- calcbot.py
+++ calcbot.py
@@ -23,41 +23,45 @@ def calc(new_str):
   
     # 4 функции для вычисления
     def div(index): # деление
-        new_list[index] = int(new_list[index - 1]) / int(new_list[index + 1])
+        new_list[index] = float(new_list[index - 1]) / float(new_list[index + 1])
         new_list.pop(index - 1)
         new_list.pop(index)
         return new_list
   
     def mult(index): # умножение
-        new_list[index] = int(new_list[index - 1]) * int(new_list[index + 1])
+        new_list[index] = float(new_list[index - 1]) * float(new_list[index + 1])
         new_list.pop(index - 1)
         new_list.pop(index)
         return new_list
   
     def add(index): # сложение
-        new_list[index] = int(new_list[index - 1]) + int(new_list[index + 1])
+        new_list[index] = float(new_list[index - 1]) + float(new_list[index + 1])
         new_list.pop(index - 1)
         new_list.pop(index)
         return new_list
   
     def subtr(index): # вычитание
-        new_list[index] = int(new_list[index - 1]) - int(new_list[index + 1])
+        new_list[index] = float(new_list[index - 1]) - float(new_list[index + 1])
         new_list.pop(index - 1)
         new_list.pop(index)
         return new_list
   
     # а тут цикл с вычиcлением; гоняет список по функциям сверху пока не придёт к ответу
-    while len(new_list) != 1: 
-        for index, item in enumerate(new_list):
-            if item == '*':
-                new_list = mult(index)
-            if item == '/':
-                new_list = div(index)
-            if '*' not in new_list and '/' not in new_list:
-                if item == '+':
-                    new_list = add(index)
-                if item == '-':
-                    new_list = subtr(index)
+    while len(new_list) != 1:
+        if '*' in new_list:
+            a = new_list.index('*')
+            new_list = mult(a, new_list)
+        if '/' in new_list:
+            a = new_list.index('/')
+            new_list = div(a, new_list)
+        if '*' not in new_list and '/' not in new_list:
+            if '+' in new_list:
+                a = new_list.index('+')
+                new_list = add(a, new_list)
+            if '-' in new_list:
+                a = new_list.index('-')
+                new_list = subtr(a, new_list)
+        print(new_list)
     
     return '{0:g}'.format(new_list[0]) #это чтобы .0 отбрасывать в конце чисел
 
