7ce52ce6c0fd470cfeeff61d00b8674974dc026c
Murloc Murlockovich <murlocked@Murlocs-MacBook-Air.local>
2022-12-15 21:17:30 +0300

    quick fix

--- calc.py
+++ calc.py
@@ -1,52 +1,54 @@
 new_str = input()
 new_list = []
+steps = []
 pos = 0
 for index, item in enumerate(new_str): # тут я разбиваю выражение на список из чисел и знаков e.g. ['14', '/', '2']
     if item == 'x' or item == 'х':
         item = '*'
     if item == '*' or item == '/' or item == '+' or item == '-':
+        steps.append(item)
         new_list.append(new_str[pos:index])
         new_list.append(item)
         pos = index + 1
 new_list.append(new_str[pos:])
   
 # 4 функции для вычисления
-def div(index, new_list): # деление
+def div(index): # деление
     new_list[index] = float(new_list[index - 1]) / float(new_list[index + 1])
     new_list.pop(index - 1)
     new_list.pop(index)
     return new_list
 
-def mult(index, new_list): # умножение
+def mult(index): # умножение
     new_list[index] = float(new_list[index - 1]) * float(new_list[index + 1])
     new_list.pop(index - 1)
     new_list.pop(index)
     return new_list
 
-def add(index, new_list): # сложение
+def add(index): # сложение
     new_list[index] = float(new_list[index - 1]) + float(new_list[index + 1])
     new_list.pop(index - 1)
     new_list.pop(index)
     return new_list
 
-def subtr(index, new_list): # вычитание
+def subtr(index): # вычитание
     new_list[index] = float(new_list[index - 1]) - float(new_list[index + 1])
     new_list.pop(index - 1)
     new_list.pop(index)
     return new_list
 
 while len(new_list) != 1:
-    if '*' in new_list:
-        a = new_list.index('*')
-        new_list = mult(a, new_list)
-    if '/' in new_list:
-        a = new_list.index('/')
-        new_list = div(a, new_list)
-    if '*' not in new_list and '/' not in new_list:
-        if '+' in new_list:
-            a = new_list.index('+')
-            new_list = add(a, new_list)
-        if '-' in new_list:
-            a = new_list.index('-')
-            new_list = subtr(a, new_list)
+    for index, step in enumerate(steps):
+        a = new_list.index(step)
+        if step == '*':
+            new_list = mult(a)
+            steps.pop(index)
+        if step == '/':
+            new_list = div(a)
+            steps.pop(index)
+        if '*' not in steps and '/' not in steps:
+            if step == '+':
+                new_list = add(a)
+            if step == '-':
+                new_list = subtr(a)
 print('{0:g}'.format(new_list[0])) #это чтобы .0 отбрасывать в конце чисел
--- calcbot.py
+++ calcbot.py
@@ -13,11 +13,13 @@ def handler(event,context):
 def calc(new_str):
     new_str = str(new_str)
     new_list = []
+    steps = []
     pos = 0
     for index, item in enumerate(new_str): # тут я разбиваю выражение на список из чисел и знаков e.g. ['14', '/', '2']
         if item == 'x' or item == 'х':
             item = '*'
         if item == '*' or item == '/' or item == '+' or item == '-':
+            steps.append(item)
             new_list.append(new_str[pos:index])
             new_list.append(item)
             pos = index + 1
@@ -50,19 +52,19 @@ def calc(new_str):
   
     # а тут цикл с вычиcлением; гоняет список по функциям сверху пока не придёт к ответу
     while len(new_list) != 1:
-        if '*' in new_list:
-            a = new_list.index('*')
-            new_list = mult(a, new_list)
-        if '/' in new_list:
-            a = new_list.index('/')
-            new_list = div(a, new_list)
-        if '*' not in new_list and '/' not in new_list:
-            if '+' in new_list:
-                a = new_list.index('+')
-                new_list = add(a, new_list)
-            if '-' in new_list:
-                a = new_list.index('-')
-                new_list = subtr(a, new_list)
+        for index, step in enumerate(steps):
+            a = new_list.index(step)
+            if step == '*':
+                new_list = mult(a, new_list)
+                steps.pop(index)
+            if step == '/':
+                new_list = div(a, new_list)
+                steps.pop(index)
+            if '*' not in steps and '/' not in steps:
+                if step == '+':
+                    new_list = add(a, new_list)
+                if step == '-':
+                    new_list = subtr(a, new_list)
         
     return '{0:g}'.format(new_list[0]) #это чтобы .0 отбрасывать в конце чисел
 
